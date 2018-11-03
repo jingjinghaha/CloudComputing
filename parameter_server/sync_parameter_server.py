b0754d253730fddd2fe5c2dea524c4f3c0a75894
Fangjing Wu <fangjing.wu@sv.cmu.edu>
2018-11-03 12:46:58 -0700

    k in loop

--- parameter_server/sync_parameter_server.py
+++ parameter_server/sync_parameter_server.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     ray.init(redis_address=args.redis_address)
-    
+
     # Create a parameter server.
     net = model.SimpleCNN()
     # net.load_model()
@@ -92,15 +92,15 @@ if __name__ == "__main__":
 
     k = args.num_workers-backups
 
-    p = np.random.randint(0,3)
-    if p == 0:
-        k = 0
-    elif p == 1:
-        k = 4
-    else:
-        k = 8
-
     while i<=1000:
+        p = np.random.randint(0,3)
+        if p == 0:
+            k = 0
+        elif p == 1:
+            k = 4
+        else:
+            k = 8
+            
         tic = time.time()
         # Compute and apply gradients.
         # compute_tasks = [worker.compute_gradients.remote(current_weights) for worker in workers]
