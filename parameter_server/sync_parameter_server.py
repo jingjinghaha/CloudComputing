44b15651218a24a691514e17e4daf7cf6d67d040
Fangjing Wu <fangjing.wu@sv.cmu.edu>
2018-12-04 18:54:38 -0800

    random normal

--- parameter_server/sync_parameter_server.py
+++ parameter_server/sync_parameter_server.py
@@ -90,8 +90,8 @@ if __name__ == "__main__":
 
     k = args.num_workers-backups
 
-    bid_price_low = 0.4086
-    bid_price_high = 0.7215
+    bid_price_low = 0.5
+    bid_price_high = 0.8
 
     # assume the spot price is between 0.2 and 1
     i = 1
@@ -103,8 +103,8 @@ if __name__ == "__main__":
     losses = []
 
     while i<=1000:
-        spot_price = np.random.uniform(low=0.2, high=1.0)
-        #spot_price = np.random.normal(loc=0.6, scale=0.175)
+        #spot_price = np.random.uniform(low=0.2, high=1.0)
+        spot_price = np.random.normal(loc=0.6, scale=0.175)
         if spot_price <= bid_price_low:
             k = 8
         elif spot_price <= bid_price_high:
